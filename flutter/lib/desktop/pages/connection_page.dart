d54c1b18ce8d74a0f3bf969724f3d8d87bbbfa9b
caxx029 <40084568+caxx029@users.noreply.github.com>
2025-12-28 07:13:01 +0800

    Remove image widget from connection_page.dart
    Removed the image widget from the connection page.

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -297,18 +297,6 @@ class _ConnectionPageState extends State<ConnectionPage>
             ).marginOnly(top: 22),
             SizedBox(height: 12),
           ],
-       children: [
-            Expanded(
-              child: Center(
-                child: Image.asset(
-                  'assets/rustdesk.png',
-                  width: 399, 
-                  height: 106, 
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-          ],       
         ).paddingOnly(left: 12.0)),
         if (!isOutgoingOnly) const Divider(height: 1),
         if (!isOutgoingOnly) OnlineStatusWidget()
